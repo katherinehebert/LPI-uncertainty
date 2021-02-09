9044b6a9f710d8f77a82c3995e8db06632e1c6fa
Katherine Hébert <katherine.ahebert@gmail.com>
2021-02-09 15:39:13 -0500

    lowered the slope for carrying capacity change to avoid negative pops

--- scripts/run_sims.R
+++ scripts/run_sims.R
@@ -1,3 +1,6 @@
+# Script to run simulations for scenarios of population change and calculate LPI 
+# for each scenario
+
 # generate the scenarios
 source('scripts/scenario_functions.R')
 source('scripts/sim_mech.R')
@@ -16,9 +19,12 @@ proc = 0.1
 obs = 10
 
 ## CARRYING CAPACITY SCENARIOS ##  ------
-K_increase = 100 + 5*c(0:9)
+K_increase = 100 + 4*c(0:9)
 K_stable = rep(100, 10)
-K_decline = 100 - 5*c(0:9)
+K_decline = 100 - 4*c(0:9)
+# plot these!
+
+## no covariance from interactions ## ------------------------------------------
 
 ## scenario 1: extreme decline, no covariance, no lag  ------
 sim_mech(
@@ -40,21 +46,103 @@ make_gam("scenario1")
 get_lpi("scenario1")
 
 
-# scenario 1: extreme decline, no covariance, no lag
+# scenario 2: stable, no covariance, no lag ------
 sim_mech(
   n_pairs = pop_pairs, timesteps = steps,
   N0i = 100, N0j = 100,
   lambda_i = 1.5, lambda_j = 1.5,
   alpha_ij = 0, alpha_ji = 0,
   process = proc, observation = obs,
-  K = K_decline,
-  "test"
+  K = K_stable,
+  "scenario2"
 )
 make_true(n_pairs = pop_pairs, timesteps = steps,
           N0i = 100, N0j = 100,
           lambda_i = 1.5, lambda_j = 1.5,
           alpha_ij = 0, alpha_ji = 0,
           K = K_decline,
-          "test")
-make_gam("test")
-get_lpi("test")
+          "scenario2")
+make_gam("scenario2")
+get_lpi("scenario2")
+
+
+# scenario 3: growth, no covariance, no lag ------
+sim_mech(
+  n_pairs = pop_pairs, timesteps = steps,
+  N0i = 100, N0j = 100,
+  lambda_i = 1.5, lambda_j = 1.5,
+  alpha_ij = 0, alpha_ji = 0,
+  process = proc, observation = obs,
+  K = K_increase,
+  "scenario3"
+)
+make_true(n_pairs = pop_pairs, timesteps = steps,
+          N0i = 100, N0j = 100,
+          lambda_i = 1.5, lambda_j = 1.5,
+          alpha_ij = 0, alpha_ji = 0,
+          K = K_increase,
+          "scenario3")
+make_gam("scenario3")
+get_lpi("scenario3")
+
+
+## immediate covariance from interactions ## -----------------------------------
+
+## scenario 4: extreme decline, negative covariance, no lag  ------
+sim_mech(
+  n_pairs = pop_pairs, timesteps = steps,
+  N0i = 100, N0j = 100,
+  lambda_i = 1.5, lambda_j = 1.5,
+  alpha_ij = -0.01, alpha_ji = 0.01, # covariance happens here - antagonistic interaction
+  process = proc, observation = obs,
+  K = K_decline,
+  "scenario4"
+)
+make_true(n_pairs = pop_pairs, timesteps = steps,
+          N0i = 100, N0j = 100,
+          lambda_i = 1.5, lambda_j = 1.5,
+          alpha_ij = -0.01, alpha_ji = 0.01, # covariance happens here - antagonistic interaction
+          K = K_decline,
+          "scenario4")
+make_gam("scenario4")
+get_lpi("scenario4")
+
+
+# scenario 5: stable, no covariance, no lag ------
+sim_mech(
+  n_pairs = pop_pairs, timesteps = steps,
+  N0i = 100, N0j = 100,
+  lambda_i = 1.5, lambda_j = 1.5,
+  alpha_ij = 0, alpha_ji = 0,
+  process = proc, observation = obs,
+  K = K_stable,
+  "scenario2"
+)
+make_true(n_pairs = pop_pairs, timesteps = steps,
+          N0i = 100, N0j = 100,
+          lambda_i = 1.5, lambda_j = 1.5,
+          alpha_ij = 0, alpha_ji = 0,
+          K = K_decline,
+          "scenario2")
+make_gam("scenario2")
+get_lpi("scenario2")
+
+# 
+# # scenario 3: growth, no covariance, no lag ------
+# sim_mech(
+#   n_pairs = pop_pairs, timesteps = steps,
+#   N0i = 100, N0j = 100,
+#   lambda_i = 1.5, lambda_j = 1.5,
+#   alpha_ij = 0, alpha_ji = 0,
+#   process = proc, observation = obs,
+#   K = K_increase,
+#   "scenario3"
+# )
+# make_true(n_pairs = pop_pairs, timesteps = steps,
+#           N0i = 100, N0j = 100,
+#           lambda_i = 1.5, lambda_j = 1.5,
+#           alpha_ij = 0, alpha_ji = 0,
+#           K = K_increase,
+#           "scenario3")
+# make_gam("scenario3")
+# get_lpi("scenario3")
