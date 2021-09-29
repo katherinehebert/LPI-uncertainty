12b63e143be1ac0d937c0e5b8a2809dfc85d137f
Katherine Hébert <katherine.ahebert@gmail.com>
2021-09-29 14:32:49 -0400

    initial commit

---
+++ scripts/find_errorsource.R
@@ -0,0 +1,100 @@
+## checks: where does the error come from in the LPI calculation?
+
+# load packages and prep environment
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(data.table)
+library(patchwork)
+theme_set(ggpubr::theme_pubr())
+
+# import expected uncertainties of each scenario's growth rates
+uncertainties <- lapply(paste0("outputs/", list.files(path = "outputs/", pattern = "_uncertainty.RDS")), readRDS)
+names(uncertainties) <- gsub("_uncertainty.RDS", "", list.files(path = "outputs/", pattern = "_uncertainty.RDS"))
+uncertainty <- bind_rows(uncertainties, .id = "scenario")
+
+# import results
+results <- lapply(paste0("outputs/", list.files(path = "outputs/", pattern = "_results.RDS")[-1]), readRDS)
+names(results) <- gsub("_results.RDS", "", list.files(path = "results/", pattern = "_results.RDS")[-1])
+results <- bind_rows(results, .id = "scenario")
+
+# read results file
+df <- readRDS("outputs/all_results.RDS")
+
+# format interaction to show up on the plots in the right order
+df$interaction <- gsub("-0.2", "Strong Synchrony", df$interaction) 
+df$interaction <- gsub("-0.1", "Weak Synchrony", df$interaction) 
+df$interaction <- gsub("0.1", "Weak Asynchrony", df$interaction) 
+df$interaction <- gsub("0.2", "Strong Asynchrony", df$interaction) 
+df$interaction <- gsub("0", "No Synchrony", df$interaction) 
+df$interaction <- factor(df$interaction, 
+                         levels = c("Strong Asynchrony", "Weak Asynchrony", "No Synchrony", "Weak Synchrony", "Strong Synchrony"))
+# same with process error
+df$Process_error <- factor(df$Process_error, levels = c("0", "0.1", "0.2"))
+
+# combine with results including standard error for each step
+results$scenario <- uncertainty$scenario
+df_err <- inner_join(results, df)
+
+## comparing GAM predictions to simulated population sizes
+
+# plot difference between prediction and simulation
+ggplot(filter(df_err, Lag == "0")) +
+  geom_jitter(aes(y = N_pred-N, 
+                  x = interaction, 
+                  col = direction), size = 1, alpha = .7) +
+  facet_wrap(~Process_error, dir = "v") +
+  labs(x = "", y = expression(N[sim]~Delta~N[GAM]), col = "Scenario") +
+  theme(legend.position = "bottom")
+ggsave("figures/figsupp_GAMpredictions.png", width = 11.8, height = 7)
+
+# plot standard error from the GAM predictions
+ggplot(filter(df_err, Lag == "0")) +
+  geom_jitter(aes(y = N_se, 
+                  x = interaction, 
+                  col = direction), 
+              size = 1, alpha = .2,
+              position = position_jitterdodge()) +
+  geom_boxplot(aes(y = N_se, 
+                  x = interaction, 
+                 fill = direction), alpha = .6, outlier.shape = NA) +
+  facet_wrap(~Process_error, dir = "v", scales = "free") +
+  labs(x = "", y = "GAM standard error", fill = "Scenario", col = "Scenario") +
+  theme(legend.position = "bottom")
+ggsave("figures/figsupp_GAMerror.png", width = 7, height = 8.79)
+
+# the bootstrapped confidence intervals always underestimate the error-propagated intervals
+a <- ggplot(filter(df_err, Lag == "0")) +
+  geom_jitter(aes(y = cilo_se-cilo_boot, 
+                  x = interaction, 
+                  col = direction), 
+              size = 1, alpha = .2,
+              position = position_jitterdodge()) +
+  geom_boxplot(aes(y = cilo_se-cilo_boot, 
+                   x = interaction, 
+                   fill = direction), alpha = .6, outlier.shape = NA) +
+  facet_wrap(~Process_error, dir = "v") +
+  labs(title = "Lower confidence interval",
+       x = "", 
+       y = expression(Delta~CI[error]-CI[bootstrap]), 
+       fill = "Scenario", col = "Scenario") +
+  theme(legend.position = "bottom")
+
+b <- ggplot(filter(df_err, Lag == "0")) +
+  geom_jitter(aes(y = cihi_se-cihi_boot, 
+                  x = interaction, 
+                  col = direction), 
+              size = 1, alpha = .2,
+              position = position_jitterdodge()) +
+  geom_boxplot(aes(y = cihi_se-cihi_boot, 
+                   x = interaction, 
+                   fill = direction), alpha = .6, outlier.shape = NA) +
+  facet_wrap(~Process_error, dir = "v") +
+  labs(title = "Upper confidence interval",
+       x = "", 
+       y = expression(Delta~CI[error]-CI[bootstrap]), 
+       fill = "Scenario", col = "Scenario") +
+  theme(legend.position = "bottom")
+a + b
+ggsave("figures/figsupp_GAMvsBoot_CI.png", width = 17.3, height = 7)
+
