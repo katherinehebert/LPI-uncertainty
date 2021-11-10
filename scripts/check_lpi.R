c94fb6102bbcd96c01d014ff28ce7bd5def1b11e
Katherine Hébert <katherine.ahebert@gmail.com>
2021-11-10 15:14:42 -0500

    add function to calculate the true LPI with rlpi

--- scripts/check_lpi.R
+++ scripts/check_lpi.R
@@ -124,4 +124,102 @@ sim_names <- c(paste0("scenario1", LETTERS[1:9]),
               paste0("scenario7", LETTERS[1:18]))
 # apply to all simulations
 lapply(sim_names, lpi_checker)              
+
                
+### calculate true LPI with rlpi
+
+lpi_truechecker <- function(scenario_name){
+  
+  # import simulated population
+  sim <- readRDS(paste0("~/Documents/GitHub/LPI-sensitivity/outputs/", scenario_name, "_true.RDS"))
+  
+  # change time column to fake years, for the rlpi function to work
+  sim$time <- paste0("X", sim$time + 1969)
+  
+  # convert to wide format
+  sim <- subset(sim, select = -dt)
+  simw <- pivot_wider(sim, names_from = time, values_from = N)
+  
+  # create ID and Binomial columns
+  simw$ID <- 1:2
+  simw$Binomial <- LETTERS[1:2]
+  
+  # Constructing infiles from a populations table
+  
+  # Select populations in the dataset by setting all to TRUE
+  index_vector = rep(TRUE, nrow(simw))
+  
+  # Create infile for Canada populations
+  checker_infile_name <- create_infile(simw, 
+                                       index_vector = index_vector, 
+                                       name = "truechecker", 
+                                       start_col_name = "X1970", 
+                                       end_col_name = "X1979")
+  
+  
+  # index with 1000 bootstraps (1137 pops) without weightings 
+  lpi <- LPIMain(checker_infile_name, 
+                 REF_YEAR = 1970, PLOT_MAX = 1980, 
+                 BOOT_STRAP_SIZE = 1000, 
+                 use_weightings=0, 
+                 VERBOSE=FALSE, save_plots = 0, plot_lpi = 0)
+  # Remove NAs (trailing years with no data)
+  lpi <- lpi[complete.cases(lpi), ]
+  lpi$time <- rownames(lpi) %>% as.numeric()
+  saveRDS(lpi, paste0("outputs/", scenario_name, "_true_rlpi.RDS"))
+  
+  # plot this LPI
+  ggplot(data = lpi, aes(x = time)) +
+    geom_ribbon(data = lpi, aes(ymin = CI_low, 
+                                ymax = CI_high), alpha = .2, fill = "navyblue") +
+    geom_line(data = lpi, aes(y = LPI_final), col = "navyblue") +
+    labs(x = "", y = "LPI") +
+    theme(legend.position = "none")
+  ggsave(paste0("figures/", scenario_name, "_true_rlpi.png"))
+  
+  
+  ## check the difference between LPI and my calculated version...
+  
+  sim_lpi <- readRDS(paste0("~/Documents/GitHub/LPI-sensitivity/outputs/", scenario_name, "_lpi.RDS"))
+  sim_lpi$time <- sim_lpi$time + 1969
+  
+  # Plot the resulting indices to compare them, and save it
+  
+  # make palette with names
+  pal = c("rLPI" = "red", "myLPI" = "blue")
+  ggplot() +
+    # rlpi LPI
+    geom_line(data = lpi, aes(x = time, 
+                              y = LPI_final, 
+                              col = "rLPI")) + 
+    # my calculation
+    geom_line(data = sim_lpi, aes(x = time, 
+                                  y = LPI_boot, 
+                                  col = "myLPI")) +
+    scale_fill_manual(values = pal) +
+    scale_color_manual(values = pal) +
+    labs(fill = "Source", col = "Source")
+  ggsave(paste0("figures/", scenario_name, "_lpicheck_true.png"))
+  
+  
+  # make a table with the results
+  checker_df <- data.frame(
+    "scenario" = scenario_name,
+    "LPI_diff" = sim_lpi$LPI_boot - lpi$LPI_final,
+    "time" = sim_lpi$time - 1969
+  )
+  saveRDS(checker_df, paste0("outputs/", scenario_name, "_lpicheck_true.RDS"))
+  
+}
+
+# get all scenario names
+sim_names <- c(paste0("scenario1", LETTERS[1:9]),
+               paste0("scenario2", LETTERS[1:18]),
+               paste0("scenario3", LETTERS[1:18]),
+               paste0("scenario4", LETTERS[1:18]),
+               paste0("scenario5", LETTERS[1:18]),
+               paste0("scenario6", LETTERS[1:18]),
+               paste0("scenario7", LETTERS[1:18]))
+lapply(paste0("scenario2", LETTERS[11:18]), lpi_truechecker)
+
+# fails: A, D, G, J, M, P - to do: figure out why
