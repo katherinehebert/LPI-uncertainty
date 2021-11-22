201b1db16701760085d0e2b9e0e82207907f5163
Katherine Hébert <katherine.ahebert@gmail.com>
2021-11-22 11:42:27 -0500

    set same seed as sim_mech + add log-ratio calculation + final edits and clean up

--- scripts/make_true.R
+++ scripts/make_true.R
@@ -16,13 +16,16 @@
 
 make_true <- function(
   n_pairs = 10, 
-  timesteps = 10, 
+  timesteps = 11, 
   N0i = 100, N0j = 100, 
   lambda_i = 1.5, lambda_j = 1.5, 
   alpha_ij, alpha_ji,
   K,
   lag_value,
   simname = filename){
+  
+  # set seed for randomisations
+  set.seed(2)
   
   ## arguments: ## ----
   
@@ -39,10 +42,6 @@ make_true <- function(
   # add more time steps to allow lag
   timesteps = timesteps + lag_value
   
-  # make growth rates matrix form
-  r_i = matrix(lambda_i, nrow = n_pairs, ncol = timesteps) 
-  r_j = matrix(lambda_i, nrow = n_pairs, ncol = timesteps) 
-  
   # run simulation --------------------------------------------------------------- 
   
   # initialize matrix to store results (population sizes)
@@ -54,41 +53,44 @@ make_true <- function(
   for(t in 1:timesteps){
     
     # population i
-    temp_i = Ni[t]*(1 + r_i[,t]*(1 - (Ni[t] + alpha_ij*Nj[t])/K[t])) 
+    temp_i = Ni[,t]*(1 + lambda_i*(1 - (Ni[,t] + alpha_ij*Nj[,t])/K[t]))
+
     # if NAs or 0, set to 0.
     temp_i[which(is.na(temp_i))] <- 0
     temp_i[which(temp_i < 0)] <- 0
     Ni <- cbind(Ni, temp_i) # append resulting population size to results vector
     
     # population j
-    temp_j = Nj[t]*(1 + r_j[,t]*(1 - (Nj[t] + alpha_ji*Ni[t])/K[t])) 
+    temp_j = Nj[,t]*(1 + lambda_j*(1 - (Nj[,t] + alpha_ji*Ni[,t])/K[t]))
+
     # if NAs or 0, set to 0.
     temp_j[which(is.na(temp_j))] <- 0
     temp_j[which(temp_j < 0)] <- 0
     Nj <- cbind(Nj, temp_j) # append resulting population size to results vector
   }
-  Ni <- Ni[,-1] # remove first column, which is only needed to start the calculation
-  Nj <- Nj[,-1]
   
   # introduce lag to populations j
   if(lag_value != 0){
     # print("running") # to test the loop
     Nj <- Nj[,1:(lag_value + 1)]
+    dt_j <- matrix(NA, nrow = n_pairs, ncol = timesteps)
+    
     for(t in c(lag_value + 1):timesteps){
+      
       # population j
-      temp_j = Nj[,t]*(1 + r_j[,t]*(1 - (Nj[,t] + alpha_ji*Ni[,(t-lag_value)])/K[t]))
+      temp_j = Nj[,t]*(1 + lambda_j*(1 - (Nj[,t] + alpha_ji*Ni[,(t-lag_value)])/K[t]))
       temp_j[which(is.na(temp_j))] <- 0
       temp_j[which(temp_j < 0)] <- 0
-      #print(temp_j)
       Nj <- cbind(Nj, temp_j)
     }
+    
   }
-  
+
   # remove extra steps from introduced lag
-  timesteps = timesteps-lag_value
-  Ni <- Ni[,c(1:timesteps)]
-  Nj <- Nj[,c(1:timesteps)]
-  
+  timesteps <- timesteps - lag_value
+  Ni <- Ni[,c(2:(timesteps+1))]
+  Nj <- Nj[,c(2:(timesteps+1))]
+
   # plot results -----------------------------------------------------------------
   
   # create vector of time values for plotting
@@ -111,13 +113,25 @@ make_true <- function(
   # bind together
   N = rbind(pops_long(Ni, set_id = "i"), pops_long(Nj, set_id = "j"))
   
-  true <- filter(N, pop == "pop1")
-  true$dt <- 0  
-  for(i in 2:10){
-    x <- filter(true, popID == "i-pop1")
-    y <- filter(true, popID == "j-pop1")
-    true$dt[i] <- log10(x$N[i]/x$N[i-1])
-    true$dt[i+10] <- log10(y$N[i]/y$N[i-1])
+  true <- N
+
+  calc_dt_chain <- function(x){
+    
+    N <- x$N
+    
+    # calculate population growth rate (chain method)
+    dt = c(1) # initial value
+    for(i in 2:length(N)){
+      dt[i] = log10(N[i]/N[i-1])
+    }
+    x$dt <- dt
+    return(x)
   }
+  
+  # get annual mean growth rate for each population
+  true <- true %>% dplyr::group_by(set, pop) %>% group_split() %>%
+    lapply(calc_dt_chain) %>% bind_rows()
+  
+  # save output
   saveRDS(true, paste0("outputs/", simname, "_true.RDS"))
 }
